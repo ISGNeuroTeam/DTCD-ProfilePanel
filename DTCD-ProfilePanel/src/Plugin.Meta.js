eca4b94967956065030c5c3d37bcbcb8e36d187a
rkuramshin <rkuramshin@isgneuro.com>
2022-04-12 13:19:06 +0400

    inited panel

---
+++ DTCD-ProfilePanel/src/Plugin.Meta.js
@@ -0,0 +1,9 @@
+import { version } from '../package.json';
+
+export default {
+  type: 'panel',
+  name: 'ProfilePanel',
+  title: 'Панель профиля',
+  version,
+  withDependencies: true,
+};
---
+++ DTCD-ProfilePanel/src/PluginComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>profile panel</div>
+</template>
+
+<script>
+export default {
+  name: 'ProfilePanel',
+};
+</script>
+
+<style lang="sass" scoped>
+</style>
