f4cdc78af996e6c93b7ada87b3ca31008b57337d
JuliaGinkul <ginkul18@gmail.com>
2022-04-20 17:26:56 +0300

    minor changes

--- DTCD-ProfilePanel/src/PluginComponent.vue
+++ DTCD-ProfilePanel/src/PluginComponent.vue
@@ -171,13 +171,15 @@
           <h2 class="Subtitle">Основная информация</h2>
           
           <base-file-loader
+            label="Фото"
             description="Загрузить изображение"
-            class="LoadImage">
-            <span slot="label">Фото</span>
+            class="LoadImage"
+          >
             <svg slot="icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">    
               <path d="M19 21H5C3.89543 21 3 20.1046 3 19V5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21ZM5 5V19H19V5H5ZM18 17H6L9 13L10 14L13 10L18 17ZM8.5 11C7.67157 11 7 10.3284 7 9.5C7 8.67157 7.67157 8 8.5 8C9.32843 8 10 8.67157 10 9.5C10 10.3284 9.32843 11 8.5 11Z" fill="#938FA0"/>
             </svg>
           </base-file-loader>
+
           <span class="Annotation">Загрузите изображение в формате JPEG или PNG. Максимальный размер файла не должен превышать 10mb.</span>
 
           <base-input 
@@ -548,11 +550,6 @@ export default {
     display: flex;
     flex-direction: column;
     margin-bottom: 4px;
-
-    & > * {
-      font-weight: 700;
-      margin-bottom: 4px;
-    }
   }
 
   &.withOpenedSidebar {
